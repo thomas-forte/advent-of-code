5764ebfedcc37c71be4a675819c80a787382c3aa
Thomas Forte <tjf3191@gmail.com>
2021-12-06 18:35:44 -0500

    three

---
+++ 3.py
@@ -0,0 +1,32 @@
+gamma = ''
+epsilon = ''
+
+def process_command(command):
+    command = command.strip().split(' ')
+    return [command[0], int(command[1])]
+
+with open('3.txt') as f:
+    lines = f.readlines()
+
+
+number_length = len(lines[0].strip())
+total_numbers = len(lines)
+
+print(range(number_length))
+for x in range(number_length):
+    zero_count = 0
+    for line in lines:
+        if line[x] == '0':
+            zero_count += 1
+    if zero_count / total_numbers > .5:
+        gamma += '0'
+        epsilon += '1'
+    else:
+        gamma += '1'
+        epsilon += '0'
+
+gamma_number = int(gamma, 2)
+epsilon_number = int(epsilon, 2)
+print(f'gamma {gamma} => {gamma_number}')
+print(f'epsilon {epsilon} => {epsilon_number}')
+print(f'power consumption {gamma_number * epsilon_number}')
