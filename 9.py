fe0ac7f3741c7c75f6e05c51eb385dc0a6c50a15
Thomas Forte <tjf3191@gmail.com>
2021-12-13 19:08:02 -0500

    nine

---
+++ 9.py
@@ -0,0 +1,19 @@
+with open('9.txt') as f:
+    data = f.read().splitlines()
+
+depths = []
+for line in data:
+    depths.append(list(map(int, list(line))))
+
+points = []
+for y in range(len(depths)):
+    for x in range(len(depths[y])):
+        if y - 1 < 0 or depths[y-1][x] > depths[y][x]:
+            if y + 1 >= len(depths) or depths[y+1][x] > depths[y][x]:
+                if x - 1 < 0 or depths[y][x-1] > depths[y][x]:
+                    if x + 1 >= len(depths) or depths[y][x+1] > depths[y][x]:
+                        points.append([x, y])
+
+point_values = list(map(lambda p: depths[p[1]][p[0]] + 1, points))
+
+print(f'The sum of all the risk values is: {sum(point_values)}')
